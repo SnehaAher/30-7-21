72db0b16b911fb3fc47d80d951d1d592b931782d
SnehaAher <88182507+SnehaAher@users.noreply.github.com>
2021-07-30 13:04:34 +0530

    Update GitHubDemo.java

--- 30-7-21/src/GitHubDemo.java
+++ 30-7-21/src/GitHubDemo.java
@@ -2,5 +2,10 @@
 public class GitHubDemo {
 public static void main(String[] args) {
 	System.out.println("My First Git Hub Document");
+	public void m1()
+	{
+	System.out.println("First Task")
+	}
+	
 }
 }
