773078b5890ccbabaec0e0c1238aba9c8a269f32
sneha <sneha@Snehal>
2021-07-30 13:09:54 +0530

    second task

--- 30-7-21/src/GitHubDemo.java
+++ 30-7-21/src/GitHubDemo.java
@@ -1,11 +1,14 @@
 
 public class GitHubDemo {
-public static void main(String[] args) {
-	System.out.println("My First Git Hub Document");
+	
 	public void m1()
 	{
-	System.out.println("First Task")
+	System.out.println("First Task");
 	}
+	
+public static void main(String[] args) {
+	System.out.println("My First Git Hub Document");
+	
 	
 }
 }
